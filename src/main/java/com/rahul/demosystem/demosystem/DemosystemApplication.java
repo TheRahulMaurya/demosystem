a0aa391634b22c0c282b51b2d7793b5143ec58e3
RAHUL <r.k.mauryadv@gmail.com>
2018-10-24 23:24:55 +0530

    Initial commit

---
+++ src/main/java/com/rahul/demosystem/demosystem/DemosystemApplication.java
@@ -0,0 +1,12 @@
+package com.rahul.demosystem.demosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rahul/demosystem/demosystem/DemosystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rahul.demosystem.demosystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemosystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
