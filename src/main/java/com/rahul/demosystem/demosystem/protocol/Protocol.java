a5673a1bb439dc042b1e6edcdc44f76fbf0c2cc6
RAHUL <r.k.mauryadv@gmail.com>
2018-10-26 18:40:20 +0530

    Added ManyToOne annotation for protocol to device

--- src/main/java/com/rahul/demosystem/demosystem/protocol/Protocol.java
+++ src/main/java/com/rahul/demosystem/demosystem/protocol/Protocol.java
@@ -1,13 +1,19 @@
 package com.rahul.demosystem.demosystem.protocol;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.validator.constraints.Length;
 
+import com.rahul.demosystem.demosystem.device.Device;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -17,7 +23,6 @@ import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
 @Entity
-@Table(name = "protocols")
 public class Protocol {
 
     @Id
@@ -28,7 +33,14 @@ public class Protocol {
 
     @NotEmpty(message = "The title can't be empty")
     @Length(max = 200, message = "Title must be less than 200 characters.")
+    @Column(nullable = false)
     private String title;
+    
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "device_id")
+    private Device device;
+    
 
 
     @Temporal(TemporalType.TIMESTAMP)
@@ -37,12 +49,12 @@ public class Protocol {
 
 
     @NotEmpty(message = "The title can't be empty")
-    @Column(name = "effective_date")
+    @Column(name = "effective_date", nullable = false)
     private Date effectiveDate;
 
 
 
-    /********************************* Constructor *********************************/
+    /********************************* Constructors *********************************/
 
     public Protocol(){}
 
@@ -51,8 +63,20 @@ public class Protocol {
         this.title = title;
         this.effectiveDate = effectiveDate;
     }
+    
+    
 
-    /***************************** Getters & Setters *****************************/
+    public Protocol(int protocolId, String title, Device device, Date createdOn, Date effectiveDate) {
+		super();
+		this.protocolId = protocolId;
+		this.title = title;
+		this.device = device;
+		this.createdOn = createdOn;
+		this.effectiveDate = effectiveDate;
+	}
+
+	/***************************** Getters & Setters *****************************/
+
     public int getProtocolId() {
         return protocolId;
     }
@@ -68,8 +92,18 @@ public class Protocol {
     public void setTitle(String title) {
         this.title = title;
     }
+    
+    
 
-    public Date getCreatedOn() {
+    public Device getDevice() {
+		return device;
+	}
+
+	public void setDevice(Device device) {
+		this.device = device;
+	}
+
+	public Date getCreatedOn() {
         return createdOn;
     }
 
