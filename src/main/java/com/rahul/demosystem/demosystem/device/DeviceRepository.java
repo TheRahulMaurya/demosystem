f96f0c213d1d512dc82102f57375294757ca74bf
RAHUL <r.k.mauryadv@gmail.com>
2018-10-27 21:33:31 +0530

    Added findById function

--- src/main/java/com/rahul/demosystem/demosystem/device/DeviceRepository.java
+++ src/main/java/com/rahul/demosystem/demosystem/device/DeviceRepository.java
@@ -1,14 +1,13 @@
 package com.rahul.demosystem.demosystem.device;
 
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DeviceRepository extends CrudRepository<Device , Integer> {
 
-
-
+    Optional<Device> findByDeviceId(int deviceId);
 }
