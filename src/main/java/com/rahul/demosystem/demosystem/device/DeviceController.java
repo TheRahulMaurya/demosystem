b93b35cc5f90daf8013a8197001d519723d05157
RAHUL <r.k.mauryadv@gmail.com>
2018-10-27 21:32:54 +0530

    Defines GET,POST,PUT,DELETE functional part

--- src/main/java/com/rahul/demosystem/demosystem/device/DeviceController.java
+++ src/main/java/com/rahul/demosystem/demosystem/device/DeviceController.java
@@ -1,9 +1,15 @@
 package com.rahul.demosystem.demosystem.device;
 
+import com.rahul.demosystem.demosystem.device.service.DeviceService;
+import com.rahul.demosystem.demosystem.protocol.ProtocolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
+import com.rahul.demosystem.demosystem.protocol.Protocol;
+
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/device")
@@ -12,19 +18,80 @@ public class DeviceController {
     @Autowired
     DeviceRepository deviceRepository ;
 
+    @Autowired
+    DeviceService deviceService;
 
+    @Autowired
+    ProtocolRepository protocolRepository;
+
+
+
+    //get a user by id
     @ResponseBody
-    @RequestMapping(value="/create" , method = RequestMethod.POST)
-    public Iterable<Device> createDevice(Device device)
+    @GetMapping("/{deviceId}")
+    public Device getDevice(@PathVariable ("deviceId") int deviceId)
+    {
+        Device device = deviceRepository.findById(deviceId).orElse(new Device());
+        return device;
+    }
+
+
+
+    //view all users at once
+    @ResponseBody
+    @GetMapping("/all")
+    public Iterable<Device> allDevice()
+    {
+        Iterable<Device> devices = deviceRepository.findAll();
+        return devices;
+    }
+
+
+    //create a new device
+    @PostMapping("/create")
+    public Iterable<Device> createDevice(@RequestBody Device device)
     {
 
+    	//If a user is assign to a device status will change
+    	if(device.getUsers() != null)
+    	{
+    		device.setStatus(Status.Working);
+    	}
+    	
         deviceRepository.save(device);
         Iterable<Device> devices = deviceRepository.findAll();
         return devices;
     }
 
+
+    //Assigning protocol to device via divice id
     @ResponseBody
-    @RequestMapping(value="delete/{deviceId}" , method = RequestMethod.DELETE)
+    @PutMapping("/assignProtocol/{deviceId}")
+    public Iterable<Device> assignProtocol(@PathVariable("deviceId") int deviceId, @RequestBody Protocol protocol) throws IOException {
+
+        Device existingDevice = deviceRepository.findById(deviceId).orElse(null);
+
+        //checks either device exist or not
+        if(existingDevice == null)
+            throw new IOException("Device doesn't exist");
+
+
+        //assign protocol to device
+        List<Protocol> protocols = deviceService.assignProtocolToDevice(existingDevice , protocol);
+        
+        existingDevice.setProtocols(protocols);
+        
+        deviceRepository.save(existingDevice);
+
+        Iterable<Device> devices = deviceRepository.findAll();
+        return devices;
+
+    }
+
+
+    //delete device by id
+    @ResponseBody
+    @DeleteMapping("delete/{deviceId}")
     public Iterable<Device> deleteDevice(@PathVariable ("deviceId") int deviceId)
     {
         deviceRepository.deleteById(deviceId);
@@ -32,19 +99,5 @@ public class DeviceController {
         return devices;
     }
 
-    @ResponseBody
-    @RequestMapping(value="/{deviceId}" , method = RequestMethod.GET)
-    public Device getDevice(@PathVariable ("deviceId") int deviceId)
-    {
-        Device device = deviceRepository.findById(deviceId).orElse(new Device());
-        return device;
-    }
 
-    @ResponseBody
-    @RequestMapping(value="/all" , method = RequestMethod.GET)
-    public Iterable<Device> allDevice()
-    {
-        Iterable<Device> devices = deviceRepository.findAll();
-        return devices;
-    }
 }
